bbb7462aa204c784ab1ffab31c1bc4982ccdd755
Sebastián Orellana Pinto <se.orellanapinto@gmail.com>
2025-05-16 14:24:04 +0200

    test

---
+++ pokemon/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+from pokemon.views import PokemonViewSet
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_pokemon_list_url(self):
+        """
+        Prueba la URL del listado de Pokémon.
+        """
+        url = reverse('pokemon-list')
+        self.assertEqual(resolve(url).func.cls, PokemonViewSet)
+
+    def test_pokemon_detail_url(self):
+        """
+        Prueba la URL del detalle de un Pokémon.
+        """
+        url = reverse('pokemon-detail', args=[1])
+        self.assertEqual(resolve(url).func.cls, PokemonViewSet)
