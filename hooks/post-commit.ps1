e120ec50c6ef36b311c529f0fcf7c8d2cb19dc07
Sebastián Orellana Pinto <se.orellanapinto@gmail.com>
2025-05-16 18:28:39 +0200

    feat: prepushh

--- hooks/post-commit.ps1
+++ hooks/post-commit.ps1
@@ -1,7 +1,5 @@
-# post-commit.ps1
 Write-Host "Hook post-commit activado: Realizando push automático..."
 
-# Realizamos el push automático a la rama actual
 git push origin $(git branch --show-current)
 
 if ($LASTEXITCODE -eq 0) {
