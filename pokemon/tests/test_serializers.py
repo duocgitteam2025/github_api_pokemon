d97e935608992f1ddb4fedb84517b718b6d0d1d3
sebazky <sebazky@gmail.com>
2025-06-12 23:18:24 -0400

    feat: pre-commit-done

--- pokemon/tests/test_serializers.py
+++ pokemon/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from pokemon.models import Pokemon
 from pokemon.serializers import PokemonSerializer
 
@@ -6,11 +7,7 @@ from pokemon.serializers import PokemonSerializer
 class PokemonSerializerTest(TestCase):
 
     def setUp(self):
-        self.pokemon_data = {
-            "nombre": "Pikachu",
-            "tipo": "Eléctrico",
-            "nivel": 5
-        }
+        self.pokemon_data = {"nombre": "Pikachu", "tipo": "Eléctrico", "nivel": 5}
         self.pokemon = Pokemon.objects.create(**self.pokemon_data)
         self.serializer = PokemonSerializer(instance=self.pokemon)
 
@@ -19,14 +16,13 @@ class PokemonSerializerTest(TestCase):
         Prueba que el serializador contiene los campos esperados.
         """
         data = self.serializer.data
-        self.assertEqual(set(data.keys()), set(
-            ['id', 'nombre', 'tipo', 'nivel']))
+        self.assertEqual(set(data.keys()), set(["id", "nombre", "tipo", "nivel"]))
 
     def test_field_content(self):
         """
         Prueba que los campos contienen el valor correcto.
         """
         data = self.serializer.data
-        self.assertEqual(data['nombre'], "Pikachu")
-        self.assertEqual(data['tipo'], "Eléctrico")
-        self.assertEqual(data['nivel'], 5)
+        self.assertEqual(data["nombre"], "Pikachu")
+        self.assertEqual(data["tipo"], "Eléctrico")
+        self.assertEqual(data["nivel"], 5)
